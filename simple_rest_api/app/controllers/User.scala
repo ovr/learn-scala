07de33c471aa023a7499e02ebf872d69c9d0f2e1
Dmitry Patsura <zaets28rus@gmail.com>
2015-12-06 15:33:52 +0300

    Finish to write User controller

--- simple_rest_api/app/controllers/User.scala
+++ simple_rest_api/app/controllers/User.scala
@@ -1,26 +1,43 @@
 package controllers
 
-import play.api.libs.json.{JsValue, Json}
+import play.api.db.DB
 import play.api.mvc._
+import play.api.libs.json.{JsValue, JsString, Json}
 import anorm._
+import play.api.Play.current
+import anorm.SqlParser._
+import anorm._
+
+case class UserEntity (id: Long, firstname: String)
 
 object User extends Controller {
 
   def index = Action {
-    SQL("SELECT id, firstname FROM users").map {
-      case Row(id: Long, firstname: String) => UserModel()
-
-      Ok(UserModel.toString())
+    DB.withConnection { implicit connection =>
+      val list = SQL("SELECT id, firstname FROM users").as(userParser.*).toList
+      implicit val userEntityFormat = Json.format[UserEntity]
+      Ok(Json.toJson(list))
     }
+  }
 
-    NotFound("Ooops!")
+  private val userParser = {
+    int("id") ~ get[String]("firstname") map flatten map { res =>
+      UserEntity(res._1, res._2)
+    }
   }
 
   def show(id: Long) = Action {
+    DB.withConnection { implicit connection =>
+      val user = SQL("SELECT id, firstname FROM users WHERE id = " + id).as(userParser.*)
+      implicit val userEntityFormat = Json.format[UserEntity]
 
+      if (user.isEmpty) {
+        NotFound("User with id = " + id + " doesnot exist")
+      } else {
+        Ok(Json.toJson(user))
+      }
+    }
 
-    Ok(id.toString)
+//    Ok(id.toString)
   }
 }
-
-case class UserModel()
